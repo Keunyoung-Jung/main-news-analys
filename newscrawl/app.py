ebee8d86ff9ee48ed43e5926bb87d1a73761139d
Keunyoung-Jung <jgy206@gmail.com>
2021-07-06 17:14:11 +0900

    테스트용 업데이트

---
+++ newscrawl/app.py
@@ -0,0 +1,44 @@
+from flask import Flask,request,render_template
+from twisted.internet import reactor, defer
+from newscrawl.spiders.naver import NaverSpider
+from scrapy.crawler import CrawlerRunner
+from scrapy.utils.log import configure_logging
+from mongo_ctl import get_mongo_data
+
+app = Flask(__name__)
+
+def start_spider():
+    custom_settings= {
+        'ITEM_PIPELINES' : {
+            'newscrawl.pipelines.NewscrawlPipeline': 300
+        }
+    }
+    def crawler_func():
+        configure_logging()
+        runner = CrawlerRunner(custom_settings)
+
+        @defer.inlineCallbacks
+        def crawl() :
+            yield runner.crawl(NaverSpider)
+            reactor.stop()
+        crawl()
+        reactor.run() 
+    crawler_func()
+    
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/crawl')
+def crawl():
+    if request.method == 'GET':
+        start_spider()
+        return True,200
+    return False, 400
+
+@app.route('/view')
+def view():
+    if request.method == 'GET' :
+        return get_mongo_data(),200
+    return False,400
+    
---
+++ newscrawl/mongo_ctl.py
@@ -0,0 +1,55 @@
+import pymongo
+import pandas as pd
+from collections import Counter
+from datetime import datetime
+
+class MongoDB :
+    mongodb = pymongo.MongoClient(f"mongodb://{mongo['host']}:{mongo['port']}",
+                                    username=mongo['user'],password=mongo['password'])
+
+    @classmethod
+    def conn_mongodb(cls,db_name) :
+        try:
+            cls.mongodb.admin.command('ismaster')
+            resume_db = cls.mongodb.resume_db[f'{db_name}']
+        except :
+            cls.mongodb = app.config['MONGO_CONN']
+            resume_db = cls.mongodb.resume_db[f'{db_name}']
+        return resume_db
+
+time_arr = range(24)
+time_dict = dict()
+
+def get_dataframe(cursor) :
+    return pd.DataFrame(list(cursor))
+
+def today_feature(day=str(datetime.now()).split(" ")[0]):
+    '''
+    날짜를 지정하기 위해서 다음 형식에 따라서 파라미터 입력
+    YYYY-MM-DD -> 2021-03-04
+    '''
+    for hour in time_arr :
+        naver = f'naver_{day}T{hour}'
+        
+        naver_cursor = MongoDB.conn_mongodb(naver).find()
+        time_dict[naver] = get_dataframe(naver_cursor)
+
+def keyword_top10(df) :
+    keyword_tmp = []
+    for keyword in df['keywords'] :
+        keyword_tmp += keyword
+    counts = Counter(keyword_tmp)
+    return [word[0] for word in counts.most_common(10)]
+
+def press_top3(df) :
+    return list(df['press'].value_counts().index[:3])
+
+def get_mongo_data() :
+    today_feature()
+    ret_dict = dict()
+    for date,df in time_dict.items() :
+        if len(df) != 0 :
+            ret_dict['KEYWORD_TOP10'] = keyword_top10(df)
+            ret_dict['PRESS_TOP3'] = press_top3(df)
+            
+    return ret_dict
