07bd6d4ca0a3fe7af16fa646f0610c1bc047521c
Keunyoung-Jung <jgy206@gmail.com>
2021-07-08 13:39:17 +0900

    테스트용

--- newscrawl/newscrawl/__init__.py
+++ newscrawl/newscrawl/__init__.py
@@ -8,15 +8,13 @@ def load_key(key_file) :
 
 class MongoDB :
     # mongo = load_key(key_file='/home/keyog/coding/main-news-analys/localhost_mongo_key.json')
-    mongodb = pymongo.MongoClient(f"mongodb://192.168.0.124:27017",
-                                    username='root',password='root')
+    mongodb = pymongo.MongoClient(f"mongodb://172.16.103.135:27017")
     @classmethod
     def conn_mongodb(cls,db_name) :
         try:
             cls.mongodb.admin.command('ismaster')
             collection = cls.mongodb.newscrawl[f'{db_name}']
         except :
-            cls.mongodb = pymongo.MongoClient(f"mongodb://192.168.0.124:27017",
-                                    username='root',password='root')
+            cls.mongodb = pymongo.MongoClient(f"mongodb://172.16.103.135:27017")
             collection = cls.mongodb.newscrawl[f'{db_name}']
         return collection
